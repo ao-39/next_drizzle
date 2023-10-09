a1d185e363fdb4e7b35d6d758b0afd8e39c7ee91
ao-39 <fukahireee@outlook.jp>
2023-10-09 18:18:24 +0900

    :sparkles: dbクライアントを追加した

---
+++ db/db.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as dotenv from "dotenv";
+
+import * as schema from "./schema";
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { logger: true, schema });
