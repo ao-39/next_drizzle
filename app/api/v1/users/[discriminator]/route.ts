6ca941fdc95b3066310c0ff08eac1895d66abf45
ao-39 <fukahireee@outlook.jp>
2023-10-11 00:46:07 +0900

    :sparkles: アップデートの処理を追加した

--- app/api/v1/users/[discriminator]/route.ts
+++ app/api/v1/users/[discriminator]/route.ts
@@ -1,7 +1,7 @@
-import { db } from "@/db/db";
+import { DatabaseError, db } from "@/db/db";
 import { users } from "@/db/schema";
 import { eq } from "drizzle-orm";
-import { createSelectSchema } from "drizzle-zod";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
@@ -54,4 +54,98 @@ export async function GET(
   }
 
   return NextResponse.json({ data: selectedUser[0] });
+}
+
+export const userInsertReqSchema = createInsertSchema(users, {
+  name: z.string().min(1).max(255),
+  email: z.string().email(),
+  discriminator: z
+    .string()
+    .regex(/^[A-Za-z0-9_]+$/)
+    .min(3)
+    .max(255),
+})
+  .pick({
+    name: true,
+    email: true,
+    discriminator: true,
+    note: true,
+  })
+  .partial()
+  .refine((val) => Object.keys(val).length >= 1, {
+    message: "at least one field is required",
+  });
+
+type UserUpdateReqType = z.infer<typeof userInsertReqSchema>;
+
+export async function PATCH(
+  request: NextRequest,
+  {
+    params,
+  }: {
+    params: { discriminator: string };
+  }
+): Promise<
+  NextResponse<null | {
+    validateError?: z.ZodError<UserUpdateReqType>;
+    constraintError?: "users_discriminator_unique" | "users_email_unique";
+    error?: "not found" | "internal error";
+  }>
+> {
+  const validUpdateUser = userInsertReqSchema.safeParse(await request.json());
+
+  if (validUpdateUser.success === false) {
+    return NextResponse.json(
+      {
+        validateError: validUpdateUser.error,
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  try {
+    const res = await db
+      .update(users)
+      .set(validUpdateUser.data)
+      .where(eq(users.discriminator, params.discriminator));
+    console.log(res);
+    if (res.rowCount === 0) {
+      return NextResponse.json(
+        {
+          error: "not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+  } catch (e) {
+    // 一意制約違反の場合は409を返す
+    if (e instanceof DatabaseError) {
+      if (
+        e.constraint === "users_discriminator_unique" ||
+        e.constraint === "users_email_unique"
+      ) {
+        return NextResponse.json(
+          { constraintError: e.constraint },
+          {
+            status: 409,
+          }
+        );
+      }
+    }
+    // それ以外のエラーは500を返す
+    return NextResponse.json(
+      { error: "internal error" },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  return new NextResponse(null, {
+    status: 204,
+  });
 }
